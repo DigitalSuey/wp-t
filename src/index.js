b8559cbe5d4f83a86131f4e89d74d158a461311c
Paulo Pao <paulo@redant.com.au>
2018-05-15 10:01:07 +1000

    Setup basic webpack configuration

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import style from './styles/main.css';
+
+console.log('Entry point.');
+
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
